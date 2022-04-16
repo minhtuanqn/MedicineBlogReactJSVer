1bafe7513363bb9a7ec552d985eef65a58ab3f2e
minhtuanqn <minhtuanqn320@gmail.com>
2022-04-17 02:04:00 +0700

    update

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -5,16 +5,26 @@ export default function Settings() {
   return (
     <div className="settings">
       <div className="settingsWrapper">
-        <div className="settingTitle">
+        <div className="settingsTitle">
           <span className="settingsUpdateTitle">Update Your Account</span>
           <span className="settingsDeleteTitle">Delete Account</span>
         </div>
-        <form className="settingForm">
+        <form className="settingsForm">
           <label>Profile Picture</label>
           <div className="settingsPP">
             <img src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
-            <label></label>
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon far fa-user-circle"></i>
+            </label>
+            <input type="file" id="fileInput" style={{display: "none"}}/>
           </div>
+          <label>Username</label>
+          <input type="text" placeholder="minhtuan"/>
+          <label>Email</label>
+          <input type="text" placeholder="minhtuan@gmail.com"/>
+          <label>Password</label>
+          <input type="password"/>
+          <button className="settingsSubmit">Update</button>
         </form>
       </div>
       <SideBar />
